cd317bcf3e24580d40f7fb29f6969b1c99cba215
Victor  Hugo <victorhugorodrigues815@gmail.com>
2023-10-21 17:04:26 -0300

    ALteraçã

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,102 @@
+// const cardsData = [
+//     {
+//         imgSrc: "../imgs/visto.webp",
+//         imgAlt: "Imagem de Visto - Tipos de Vistos para quem quer trabalhar no exterior",
+//         title: "Tipos de Vistos para quem quer trabalhar no exterior",
+//     },
+//     {
+//         imgSrc: "../imgs/pc.webp",
+//         imgAlt: "Imagem do Futuro Remoto - Como Surgiu a Futuro Remoto",
+//         title: "Como Surgiu a Futuro Remoto",
+//     },
+//     {
+//         imgSrc: "../imgs/vantagens.webp",
+//         imgAlt: "Imagem do Futuro Remoto - Vantagens de Trabalhar Remotamente",
+//         title: "Vantagens de Trabalhar Remotamente",
+//     },
+//     {
+//         imgSrc: "../imgs/a.arte.webp",
+//         imgAlt: "Imagem do Futuro Remoto - A Arte da Produtividade em Casa: Estratégias para Trabalho Remoto Eficiente",
+//         title: "A Arte da Produtividade em Casa: Estratégias para Trabalho Remoto Eficiente",
+//     },
+//     {
+//         imgSrc: "../imgs/construir.uma.webp",
+//         imgAlt: "Imagem de Visto - Construindo uma Carreira Remota de Sucesso: Um Guia Passo a Passo",
+//         title: "Construindo uma Carreira Remota de Sucesso: Um Guia Passo a Passo",
+//     },
+//     {
+//         imgSrc: "../imgs/cultivando-habilid.webp",
+//         imgAlt: "Imagem do Futuro Remoto - Cultivando Habilidades para o Futuro Desenvolvimento Profissional no Trabalho Remoto",
+//         title: "Cultivando Habilidades para o Futuro Desenvolvimento Profissional no Trabalho Remoto",
+//     },
+//     {
+//         imgSrc: "../imgs/educacao-a.webp",
+//         imgAlt: "Imagem de Visto - Educação à Distância: Como se Destacar em Ambientes Virtuais de Trabalho",
+//         title: "Educação à Distância: Como se Destacar em Ambientes Virtuais de Trabalho",
+//     },
+//     {
+//         imgSrc: "../imgs/equilibrio-entre.webp",
+//         imgAlt: "Imagem do Futuro Remoto - Mantendo o Equilíbrio entre Trabalho e Vida Pessoal no Mundo do Trabalho Remoto",
+//         title: "Mantendo o Equilíbrio entre Trabalho e Vida Pessoal no Mundo do Trabalho Remoto",
+//     },
+//     {
+//         imgSrc: "../imgs/ferramentas-e-tecno.webp",
+//         imgAlt: "Imagem do Futuro Remoto - Ferramentas e Tecnologias para Facilitar o Trabalho Remoto e a Aprendizagem Online",
+//         title: "Ferramentas e Tecnologias para Facilitar o Trabalho Remoto e a Aprendizagem Online",
+//     },
+//     {
+//         imgSrc: "../imgs/dominando-trabalho.webp",
+//         imgAlt: "Imagem do Futuro Remoto - Dominando o Trabalho Remoto: Dicas Essenciais para o Sucesso Profissional",
+//         title: "Dominando o Trabalho Remoto: Dicas Essenciais para o Sucesso Profissional",
+//     },
+//     {
+//         imgSrc: "../imgs/comunicacao-efetiva.webp",
+//         imgAlt: "Imagem do Futuro Remoto - O Papel da Comunicação Efetiva no Trabalho Remoto: Dicas e Estratégias",
+//         title: "O Papel da Comunicação Efetiva no Trabalho Remoto: Dicas e Estratégias",
+//     },
+//     {
+//         imgSrc: "../imgs/ergonomia-e-saude.webp",
+//         imgAlt: "Imagem de Visto - Ergonomia e Saúde no Trabalho Remoto: Como Manter um Ambiente de Trabalho Confortável",
+//         title: "Ergonomia e Saúde no Trabalho Remoto: Como Manter um Ambiente de Trabalho Confortável",
+//     },
+// ];
+
+// function createCard(cardData) {
+//     return `
+//         <article class="card">
+//             <div class="card-content">
+//                 <div class="card-image-container">
+//                     <img src="${cardData.imgSrc}" alt="${cardData.imgAlt}" class="card-image">
+//                 </div>
+//                 <div class="card-text-container">
+//                     <h3>${cardData.title}</h3>
+//                     <a href="#" class="card-link">Leia mais</a>
+//                 </div>
+//             </div>
+//         </article>
+//     `;
+// }
+
+// function addCardsToContainer(cardsData, container) {
+//     cardsData.forEach((cardData) => {
+//         const cardHTML = createCard(cardData);
+//         container.insertAdjacentHTML("beforeend", cardHTML);
+//     });
+// }
+
+// // Obtém o container onde os cards serão adicionados
+// const cardContainer = document.getElementById("card-container");
+
+// // Cria e adiciona os cards ao container
+// addCardsToContainer(cardsData, cardContainer);
+
+
+
+const maxCardsPerPage = 6;
+const cardContainer = document.getElementById("card-container");
+const paginationContainer = document.getElementById("pagination");
 const cardsData = [
+
     {
         imgSrc: "../imgs/visto.webp",
         imgAlt: "Imagem de Visto - Tipos de Vistos para quem quer trabalhar no exterior",
@@ -59,82 +157,81 @@ const cardsData = [
         imgAlt: "Imagem de Visto - Ergonomia e Saúde no Trabalho Remoto: Como Manter um Ambiente de Trabalho Confortável",
         title: "Ergonomia e Saúde no Trabalho Remoto: Como Manter um Ambiente de Trabalho Confortável",
     },
+
 ];
 
 
-    // Função para criar um card
-    function createCard(cardData) {
-        return `
-            <article class="card">
-                <div class="card-content">
-                    <div class="card-image-container">
-                        <img src="${cardData.imgSrc}" alt="${cardData.imgAlt}" class="card-image">
-                    </div>
-                    <div class="card-text-container">
-                        <h3>${cardData.title}</h3>
-                        <a href="#" class="card-link">Leia mais</a>
-                    </div>
+let currentPage = 0;
+const totalPages = Math.ceil(cardsData.length / maxCardsPerPage);
+
+function createCard(cardData) {
+    return `
+        <article class="card">
+            <div class="card-content">
+                <div class="card-image-container">
+                    <img src="${cardData.imgSrc}" alt="${cardData.imgAlt}" class="card-image">
                 </div>
-            </article>
-        `;
-    }
+                <div class="card-text-container">
+                    <h3>${cardData.title}</h3>
+                    <a href="#" class="card-link">Leia mais</a>
+                </div>
+            </div>
+        </article>
+    `;
+}
 
-    // Obtém o container onde os cards serão adicionados
-    const cardContainer = document.getElementById("card-container");
+function renderCards(page) {
+    const start = page * maxCardsPerPage;
+    const end = (page + 1) * maxCardsPerPage;
+    const currentCards = cardsData.slice(start, end);
 
-    // Cria e adiciona os cards ao container
-    cardsData.forEach((cardData) => {
+    cardContainer.innerHTML = ""; // Limpa o contêiner de cartões antes de renderizar
+
+    currentCards.forEach((cardData) => {
         const cardHTML = createCard(cardData);
         cardContainer.insertAdjacentHTML("beforeend", cardHTML);
     });
+}
 
-
-    document.addEventListener("DOMContentLoaded", function () {
-        const buttons = document.querySelectorAll(".transparent-button");
-        const pages = document.querySelectorAll(".page");
-    
-        buttons.forEach((button) => {
-            button.addEventListener("click", (event) => {
-                const targetId = event.target.getAttribute("data-target");
-                pages.forEach((page) => {
-                    page.style.display = "none";
-                });
-                document.getElementById(targetId).style.display = "block";
-            });
+function createPagination() {
+    for (let i = 0; i < totalPages; i++) {
+        const pageNumber = i + 1;
+        const pageButton = document.createElement("button");
+        pageButton.textContent = pageNumber;
+        pageButton.classList.add("page-button");
+        pageButton.addEventListener("click", () => {
+            currentPage = i;
+            renderCards(currentPage);
         });
-    });
-    
-    $(document).ready(function() {
-        $('.transparent-button').on('click', function() {
-            // Remove a classe 'active' de todos os botões transparentes
-            $('.transparent-button').removeClass('active');
-            // Adiciona a classe 'active' apenas ao botão clicado
-            $(this).addClass('active');
-        });
-    });
-    
-    function toggleButton() {
-        if (!buttonSelected) {
-            document.getElementById('depoimentosButton').classList.add('selected-button');
-            buttonSelected = true;
-            // Execute a ação desejada aqui, por exemplo, redirecionar para 'depoimentos.html'
-            window.location.href = 'depoimentos.html';
-        }
+        paginationContainer.appendChild(pageButton);
     }
+}
 
+function initialize() {
+    createPagination();
+    renderCards(currentPage);
 
+    // Seleciona o botão "1" quando a página é carregada
+    const firstPageButton = document.querySelector(".page-button");
+    if (firstPageButton) {
+        firstPageButton.classList.add("selected");
+    }
+}
 
-// // Obtém a referência para o elemento da seta para baixo
-// const menuToggle = document.getElementById('menu-toggle');
+initialize();
 
-// // Obtém a referência para o elemento do menu suspenso
-// const dropdownMenu = document.querySelector('.dropdown-menu');
+document.addEventListener("DOMContentLoaded", function () {
+    const paginationButtons = document.querySelectorAll(".pagination .page-button");
 
-// // Adiciona um ouvinte de eventos de clique à seta para baixo
-// menuToggle.addEventListener('click', () => {
-// // Verifica se o menu está atualmente visível
-//   const isVisible = dropdownMenu.style.display === 'block';
+    paginationButtons.forEach(function (button, index) {
+        button.addEventListener("click", function () {
+            paginationButtons.forEach(function (btn) {
+                btn.classList.remove("selected");
+            });
 
-// // Alterna a visibilidade do menu
-//   dropdownMenu.style.display = isVisible ? 'none' : 'block';
-// });
+            button.classList.add("selected");
+        });
+    });
+
+    updateJobListings();
+});
--- js/login.js
+++ js/login.js
@@ -1,39 +1,59 @@
 function entrar() {
     const emailInput = document.getElementById('email');
     const senhaInput = document.getElementById('password');
-    const msgError = document.getElementById("msgError");
+    const msgError = document.getElementById('msgError');
+
+    // Verifica se os campos de e-mail e senha estão vazios
+    if (emailInput.value.trim() === '' || senhaInput.value.trim() === '') {
+        exibirMensagemDeErro('Preencha todos os campos antes de continuar.');
+        return; // Retorna imediatamente para evitar a execução subsequente
+    }
 
     const listUser = JSON.parse(localStorage.getItem('listUser')) || [];
-    
     let encontrouUsuario = false;
 
     for (const item of listUser) {
         if (emailInput.value === item.emailCad && senhaInput.value === item.senhaCad) {
             encontrouUsuario = true;
-    
-            const token = Math.random().toString(16).substring(2);
+
+            const token = gerarToken();
             localStorage.setItem('userToken', token);
-    
-            const uservalid = {
+
+            const usuarioLogado = {
                 nome: item.nomeCad,
                 email: item.emailCad,
                 senha: item.senhaCad
             };
-    
-            localStorage.setItem('userLogado', JSON.stringify(uservalid));
-            break; 
-            
+
+            localStorage.setItem('userLogado', JSON.stringify(usuarioLogado));
+            break;
         }
     }
-    
+
     if (encontrouUsuario) {
-        msgError.textContent = ''; 
-
-
-        window.location.href = '../html/blog.html';
+        limparMensagemDeErro();
+        redirecionarParaPaginaBlog();
     } else {
-        msgError.innerHTML = 'Usuário não encontrado ou senha incorreta'; // Exibe a mensagem de erro
+        exibirMensagemDeErro('Usuário não encontrado ou senha incorreta');
     }
+}
 
+function exibirMensagemDeErro(mensagem) {
+    const msgError = document.getElementById('msgError');
+    msgError.innerHTML = mensagem;
     msgError.style.display = 'block';
+}
+
+function limparMensagemDeErro() {
+    const msgError = document.getElementById('msgError');
+    msgError.textContent = '';
+    msgError.style.display = 'none';
+}
+
+function gerarToken() {
+    return Math.random().toString(16).substring(2);
+}
+
+function redirecionarParaPaginaBlog() {
+    window.location.href = '../html/blog.html';
 }
